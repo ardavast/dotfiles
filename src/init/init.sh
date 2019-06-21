295b8a6f99f15c1c01bf9090c81c5753f19709b2
Ardavast Dayleryan <ardavast@noiseoverip.com>
2019-06-22 01:28:49 +0300

    Replace Vundle with vim-plug

--- src/init/init.sh
+++ src/init/init.sh
@@ -53,5 +53,5 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 sudo chsh -s /bin/zsh ardavast
 
 # Vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
